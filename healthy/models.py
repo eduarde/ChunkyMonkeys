99acb6497306559b16d046e5a96352d0c25a86b6
Cory <corina.dalaban@gmail.com>
2016-05-13 21:04:36 +0300

    added choices for item
    added choices for item

--- healthy/models.py
+++ healthy/models.py
@@ -27,10 +27,92 @@ class Lab(models.Model):
 		return 'Lab ' + str(self.date)
 
 class Item(models.Model):
-	name = models.CharField('Name', max_length=20, blank=True, null=True)
+
+	Leucocite = 'Leucocite(WBC)'
+	Hematii = 'Hematii(RBC)'
+	Hemoglobina = 'Hemoglobina(HGB)'
+	VEM = 'VEM(volum eritocitar mediu)'
+	HEM = 'HEM(hemoglobina eritocitara medie)'
+	CHEM = 'CHEM(conc HB eritocitara medie)'
+	RDW = 'RDW'
+	Trombocite = 'Trombocite(PLT)'
+	MPW = 'MPW'
+	PDW = 'PDW'
+	Neutro = 'Neutro'
+	Lym = 'Lym'
+	Monocite = 'Monocite'
+	Eozinofile = 'Eozinofile'
+	Bazofile = 'Bazofile'
+	Creatinina = 'Creatinina serica'
+	Magneziu = 'Magneziu seric'
+	Sideremie = 'Sideremie'
+	Glicemie = 'Glicemie'
+	TGP = 'TGP'
+	TSH = 'TSH'
+	FT4 = 'FT4'
+
+	NAME_CHOICES = (
+		(Leucocite,'Leucocite'),
+		(Hematii,'Hematii(RBC)'),
+		(Hemoglobina,'Hemoglobina(HGB)'),
+		(VEM,'VEM(volum eritocitar mediu'),
+		(HEM,'HEM(hemoglobina eritocitara medie)'),
+		(CHEM,'CHEM(conc HB eritocitara medie)'),
+		(RDW, 'RDW'),
+		(Trombocite,'Trombocite'),
+		(MPW, 'MPW'),
+		(PDW,'PDW'),
+		(Neutro,'Neutro'),
+		(Lym,'Lym'),
+		(Monocite, 'Monocite'),
+		(Eozinofile, 'Eozinofile'),
+		(Bazofile, 'Bazofile'),
+		(Creatinina,'Creatinina serica'),
+		(Magneziu, 'Magneziu seric'),
+		(Sideremie, 'Sideremie'),
+		(Glicemie, 'Glicemie'),
+		(TGP, 'TGP'),
+		(TSH, 'TSH'),
+		(FT4, 'FT4'),
+	)
+
+	Hematologie = 'Hematologie'
+	Biochimie = 'Biochimie'
+	Dozari = 'Dozari hormonale, Imunologie si markeri tumorali (Chemiluminiscenta)'
+
+	CATEGORY_CHOICES = (
+		(Hematologie, 'Hematologie'),
+		(Biochimie, 'Biochimie'),
+		(Dozari, 'Dozari hormonale, Imunologie si markeri tumorali (Chemiluminiscenta)'),
+
+	)
+
+	mm3 = '10^3/mm3'
+	mm6 = '10^6/mm3'
+	gdl = 'g/dl'
+	percentage = '%'
+	fL = 'fL'
+	mgdl = 'mg/dl'
+	UL = 'U\L'
+	microUI = 'μUI\ml'
+	ng = 'ng/dl'
+
+	UM_CHOICES = (
+		(mm3, '10^3/mm3'),
+		(mm6, '10^6/mm3'),
+		(gdl, 'g/dl'),
+		(percentage, '%'),
+		(fL, 'fL'),
+		(mgdl, 'mg/dl'),
+		(UL,'U\L'),
+		(microUI, 'μUI\ml'),
+		(ng, 'ng/dl'),
+	)
+
+	name = models.CharField('Name', max_length=20, blank=False, null=False, choices=NAME_CHOICES, default=Leucocite)
 	abbr = models.CharField('Abbreviation', max_length=10, blank=True, null=True)
-	category = models.CharField('Category',max_length=20, blank=True, null=True)
-	um = models.CharField('Unit', max_length=10, blank=False, null=False)
+	category = models.CharField('Category',max_length=20, blank=False, null=False, choices=CATEGORY_CHOICES, default=Hematologie)
+	um = models.CharField('Unit', max_length=10, blank=False, null=False, choices=UM_CHOICES,default=mm3)
 
 	def __str__(self):
 		return 'Item ' + self.name
