0b72023214532b26f59b8d07eaef7f01714a4ab1
BogdanAriton <bogdan.mihail.ariton@gmail.com>
2016-05-13 22:07:03 +0300

    login
    urst momentan

--- healthy/urls.py
+++ healthy/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^home/$', views.home, name='home'),
+
 ]
